efa42fbcb1f7199416a2df25c896fd105cc49f85
Nikita Gritskevich <nikitagritskevich2804@gmail.com>
2022-03-12 17:59:53 +0300

    task with using selenium grid

--- src/test/java/com/epam/lab/seleniumGrid/test/GoogleTest.java
+++ src/test/java/com/epam/lab/seleniumGrid/test/GoogleTest.java
@@ -1,2 +1,34 @@
-package com.epam.lab.seleniumGrid.test;public class GoogleTest {
+package com.epam.lab.seleniumGrid.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+  private WebDriver driver;
+  private final String URL = "http://169.254.45.134:4444/";
+
+  @BeforeMethod
+  public void initBrowser() throws MalformedURLException {
+    driver = new RemoteWebDriver(new URL(URL), new EdgeOptions());
+  }
+
+  @Test
+  public void googleOpenTest() {
+    driver.get("https://www.google.com/");
+    Assert.assertEquals(driver.getTitle(),"Google");
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() {
+    driver.quit();
+  }
 }
